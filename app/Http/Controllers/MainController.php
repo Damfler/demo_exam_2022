d936c41873bbd11782513fedfaf175b5546070c1
Dima Gashuk <72968420+Damfler@users.noreply.github.com>
2022-05-17 15:08:53 +0300

    update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,16 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
-use Illuminate\Support\Facades\DB;
+use DB;
 
 class MainController extends Controller {
 
     public function main_page() {
-        $count = DB::table('applications')->where('status', 'Одобрено')->count();
-        $app = DB::table('applications')->where('status', 'Одобрено')->limit(4)->get();
-        return view('index', [
-            'count' => $count,
-            'app' => $app
+        $count = DB::table("applications")->where("status", "Одобрена")->count();
+        $app = DB::table("applications")->where("status", "Одобрена")->orderby("created_at", "DESC")->limit(4)->get();
+        return view("index", [
+            "count" => $count,
+            "app" => $app
         ]);
     }
 
